8baa2dbab1fa05a95908380a7ce09c8855538551
tithe <tith3@proton.me>
2024-07-07 00:24:27 +0200

    added support for modals.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 	import '../app.postcss';
-
+	
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	//modals
+	import { initializeStores, Modal } from '@skeletonlabs/skeleton';
+
+	initializeStores();
 </script>
 
-
+<Modal buttonTextCancel="Redirect me!"/>
 <!-- Page Route Content -->
 <slot />
 
