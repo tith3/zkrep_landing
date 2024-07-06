8584fbbda543c94b0e8b9fceca8246fe012a4142
tithe <tith3@proton.me>
2024-07-07 00:34:30 +0200

    Fixed mobile formatting.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,10 +41,10 @@
 
 </script>
 
-<div class="size-full bg-gradient-to-br variant-gradient-primary-secondary flex justify-center items-center py-4 px-4 sm:py-10 sm:px-16 md:px-28">
+<div class="size-full bg-gradient-to-br variant-gradient-primary-secondary flex justify-center items-center py-2 px-4 sm:py-10 sm:px-16 md:px-28">
 	<div class="card grow h-full rounded-lg flex flex-col py-12 sm:py-16 px-10 sm:px-20 sm:gap-4 gap-6 items-center sm:items-start">
 		<div class="flex flex-row gap-2 w-full items-start">
-			<img src="/images/zkrep.jpg" class="sm:size-20 size-16 rounded-full"/>
+			<img src="/images/zkrep.jpg" class="sm:size-20 size-14 rounded-full"/>
 			<h1 class="text-secondary-600 p-2 w-full text-4xl justify-center sm:justify-start my-auto font-semibold">zkReputation</h1>
 		</div>
 		<h2 class="text-2xl text-on-success-token">Unlock fair airdrops, secure credit, and compliant DeFi</h2>
@@ -52,7 +52,7 @@
 		<div class="w-full flex flex-row gap-4 h-full">
 			<div class="flex flex-col gap-4 justify-between"  >
 				<div class="flex flex-col grow size-full justify-center">
-					<p class="text-5xl font-serif  justify-self-center ">GET NOTIFIED WHEN WE WILL LAUNCH!</p>
+					<p class="sm:text-5xl text-4xl font-serif  justify-self-center ">GET NOTIFIED WHEN WE WILL LAUNCH!</p>
 				</div>
 				<div class="w-full flex flex-col gap-4 justify-end grow">
 					<BetterInput placeholder="Enter your email" onEnter={handleSetEmail} errorText={emailError} bind:value={email}/>
